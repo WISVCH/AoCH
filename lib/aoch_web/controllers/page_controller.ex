4cfe77ef8e0490ab1a0f4433c1d4185437db722d
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2024-12-04 11:56:12 +0100

    inititial commit

---
+++ lib/aoch_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule AoCHWeb.PageController do
+  use AoCHWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
