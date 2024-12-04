462843a48296851e164b340159989cd0efc43d67
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2024-12-04 23:55:13 +0100

    auto scrolling

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,10 +22,25 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+Hooks = {}
+
+Hooks.ScrollToBottom = {
+  mounted() {
+    setInterval(() => {
+      window.scrollBy({
+        top: window.innerHeight-100,
+        left: 0,
+        behavior: 'smooth' // Optional for smooth scrolling
+      });
+    }, 5000)
+  }
+}
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken}
+  params: {_csrf_token: csrfToken},
+  hooks: Hooks
 })
 
 // Show progress bar on live navigation and form submits
